cd19ed171a4db7e7d0a72c1eb1a43b3f44d07b46
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-04-15 10:56:33 +0800

    4/15 牛客每日一题

---
+++ test_4_15/test_4_15/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+//BC164 Å£Å£µÄËÄÒ¶Ãµ¹åÊý
+#include<math.h>
+
+#include<stdio.h>
+int main() {
+    int l, r;
+    scanf("%d %d", &l, &r);
+    for (int i = l; i <= r; i++) {
+        int val = i, temp = 0;
+        while (val) {
+            temp += pow(val % 10, 4);
+            val /= 10;
+        }
+        if (temp == i) printf("%d ", i);
+    }
+    return 0;
+}
+
+
