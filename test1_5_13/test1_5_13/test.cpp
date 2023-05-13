6ed31051ef7d3797c9b5d7bb76709b52070d1b11
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-05-13 18:50:06 +0800

    firstc++.cpp

---
+++ test1_5_13/test1_5_13/test.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+//myfirst.cpp --displays a message
+#include<iostream>
+int main()
+{
+	using namespace std;
+	cout << "come up and C++ me some time.";
+	cout << endl;
+	cout << "You won't regret it!" << endl;
+	return 0;
+
+}
