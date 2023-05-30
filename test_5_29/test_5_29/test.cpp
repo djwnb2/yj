6b0ccd1e57189cd739448922a8b479c13923af32
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-05-30 13:42:17 +0800

    leetcode

---
+++ test_5_29/test_5_29/test.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+#include<iostream>
+int main()
+{
+	int i;
+	double e;
+	cin >> i >> e;
+	cout << i << endl;
+	cout << "hello world" << endl;
+ }
