20342ee0a1fb5edd6604c87a11684c838e1b7d98
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-06-23 16:56:23 +0800

    leetcode

---
+++ test_6_23/test_6_23/test.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+class Solution {
+public:
+    int maximumValue(vector<string>& strs) {
+        int res = 0;
+        for (auto& s : strs) {
+            bool isDigits = true;
+            for (char& c : s) {
+                isDigits &= isdigit(c);
+            }
+            res = max(res, isDigits ? stoi(s) : (int)s.size());
+        }
+        return res;
+    }
+};
+
