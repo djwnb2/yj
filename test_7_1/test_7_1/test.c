46fbd3da6dc90374e911c7aff4bf88dd37a7a673
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-07-01 22:44:34 +0800

    1. 两数之和

---
+++ test_7_1/test_7_1/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+//1. 两数之和
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0; i < numsSize; ++i) {
+        for (int j = i + 1; j < numsSize; ++j) {
+            if (nums[i] + nums[j] == target) {
+                int* ret = malloc(sizeof(int) * 2);
+                ret[0] = i, ret[1] = j;
+                *returnSize = 2;
+                return ret;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
