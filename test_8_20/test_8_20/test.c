9bcb45dd001a22704de8f1cd21bd502e0e4393a6
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-08-20 22:10:01 +0800

    1491. 去掉最低工资和最高工资后的工资平均值

---
+++ test_8_20/test_8_20/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+int cmp(const void* a, const void* b)
+{
+    return *(int*)a - *(int*)b;
+}
+double average(int* salary, int salarySize) {
+    qsort(salary, salarySize, sizeof(int), cmp);
+    double sum = 0.0;
+    for (int i = 1; i < salarySize - 1; i++)
+    {
+        sum += salary[i];
+    }
+    return sum / (salarySize - 2);
+
+}
