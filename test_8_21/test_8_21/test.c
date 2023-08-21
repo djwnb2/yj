4fc76cc3215cffcc8c10290a95e569b2589772e4
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-08-21 22:22:53 +0800

    2337. 移动片段得到字符串

---
+++ test_8_21/test_8_21/test.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+bool canChange(char* start, char* target) {
+    int n = strlen(start);
+    int i = 0, j = 0;
+    while (i < n && j < n) {
+        while (i < n && start[i] == '_') {
+            i++;
+        }
+        while (j < n && target[j] == '_') {
+            j++;
+        }
+        if (i < n && j < n) {
+            if (start[i] != target[j]) {
+                return false;
+            }
+            char c = start[i];
+            if ((c == 'L' && i < j) || (c == 'R' && i > j)) {
+                return false;
+            }
+            i++;
+            j++;
+        }
+    }
+    while (i < n) {
+        if (start[i] != '_') {
+            return false;
+        }
+        i++;
+    }
+    while (j < n) {
+        if (target[j] != '_') {
+            return false;
+        }
+        j++;
+    }
+    return true;
+}
