7b92e9cd8ae3cfb0664eb7c73c791d02437a7081
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-08-26 14:32:23 +0800

    string第一课

---
+++ string_1/string_1/test.cpp
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+using namespace std;
+//类模板的实例化
+template<class T>
+
+//void Swap(T& left, T& right)
+//{
+//	T temp = left;
+//	left = right;
+//	right = temp;
+//}
+//void My_string()
+//{
+//	string s1="hello";
+//	string s2("hello");
+//	string ret = s1 + s2;
+//	cout << ret << endl;
+//}
+//void test_string()
+//{
+//	string s1 = "hello";
+//	//迭代器
+//	string::iterator it = s1.begin();
+//	while (it != s1.end())
+//	{
+//		*it = 'b';
+//		++it;
+//	}
+//	cout << endl;
+//	cout << s1<< endl;
+//
+//}
+//void test_string2()
+//{
+//	string s1("hello world");
+//	string::reverse_iterator rit = s1.rbegin();
+//	while (rit != s1.rend())
+//	{
+//		cout << *rit << "";
+//		++rit;
+//	}
+//	cout << endl;
+//
+//}
+//void test_string2(string&s)
+//{
+//	string::const_iterator it = s.begin();
+//	while (it != s.end())
+//	{
+//
+//	}
+//}
+int firstUniqChar(string s) {
+	int countA[26] = { 0 };
+	for (auto ch::s)
+	{
+		countA[ch - 'a']++;
+	}
+}
+
+int main()
+{
+	/*int i = 1, j = 0;
+	Swap(i, j);
+	double x = 1.1, y = 2.2;
+	Swap<double>(x, y);
+	vector<int> v;
+	v.push_back(100);*/
+	//test_string();
+
+	
+}
