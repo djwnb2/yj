3477ae71cdc2ea5767b21c033c8e91578d850ca3
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-09-05 14:01:18 +0800

    598. 范围求和 II

---
+++ test_9_5/test_9_5/test.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        int mina = m, minb = n;
+        for (const auto& op : ops)
+        {
+            mina = min(mina, op[0]);
+            minb = min(minb, op[1]);
+        }
+        return mina * minb;
+    }
+};
