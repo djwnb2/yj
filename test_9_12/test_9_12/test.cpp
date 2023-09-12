1aec951742a65a8e669799e0c676bc6ab61c5686
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-09-12 20:05:03 +0800

    剑指 Offer 03. 数组中重复的数字

---
+++ test_9_12/test_9_12/test.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+class Solution {
+public:
+    int findRepeatNumber(vector<int>& nums) {
+        int i = 0;
+        while (i < nums.size())
+        {
+            if (nums[i] == i)
+            {
+                i++;
+                continue;
+            }
+            if (nums[nums[i]] == nums[i])
+            {
+                return nums[i];
+            }
+            swap(nums[i], nums[nums[i]]);
+        }
+        return -1;
+    }
+
+};
