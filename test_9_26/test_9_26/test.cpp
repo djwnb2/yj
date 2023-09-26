dc4f5d12086f4cbf6e6b1b32bc522cc1f853ebb5
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-09-26 14:31:02 +0800

    2582. 递枕头

---
+++ test_9_26/test_9_26/test.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        time %= (n - 1) * 2;
+        return time < n ? time + 1 : n * 2 - time - 1;
+
+    }
+};
