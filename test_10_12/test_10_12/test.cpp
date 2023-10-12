e69c0d7ecdf1e7f4697b96b7a6c238f1cbaf85fe
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-10-12 18:27:46 +0800

    2562. 找出数组的串联值

---
+++ test_10_12/test_10_12/test.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+class Solution {
+public:
+    long long findTheArrayConcVal(vector<int>& nums) {
+        long long ans = 0;
+        for (int i = 0, j = nums.size() - 1; i <= j; i++, j--)
+        {
+            if (i != j)
+            {
+                ans += stoi(to_string(nums[i]) + to_string(nums[j]));
+
+            }
+            else
+            {
+                ans += nums[i];
+            }
+        }
+        return ans;
+    }
+
+};
