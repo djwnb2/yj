a8db594a227e88359f25a20665cc1797fb23d023
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-10-17 22:45:44 +0800

    260. 只出现一次的数字 III

---
+++ test_10_17/test_10_17/test.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            ++freq[num];
+        }
+        vector<int> ans;
+        for (const auto& [num, occ] : freq) {
+            if (occ == 1) {
+                ans.push_back(num);
+            }
+        }
+        return ans;
+    }
+};
