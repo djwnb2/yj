b6842af169d8a3d0fbef27953fc83557ccf3ed69
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-10-29 14:21:32 +0800

    2558. 从数量最多的堆取走礼物

---
+++ test_10_29/test_10_29/test.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        make_heap(gifts.begin(), gifts.end());
+        while (k--)
+        {
+            pop_heap(gifts.begin(), gifts.end());
+            gifts.back() = sqrt(gifts.back());
+            push_heap(gifts.begin(), gifts.end());
+        }
+        return accumulate(gifts.begin(), gifts.end(), 0LL);
+    }
+};
