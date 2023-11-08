ff6d0c954d9b76125dc94e0cea7cc749790e0dc7
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-11-08 14:52:01 +0800

    796. 子矩阵的和

---
+++ test_11_8/test_11_8/test.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+const int N = 1010;
+
+int a[N][N], s[N][N];
+int main()
+{
+    int n, m, q;
+    cin >> n >> m >> q;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            scanf("%d", &a[i][j]);
+            s[i][j] = s[i][j - 1] + s[i - 1][j] - s[i - 1][j - 1] + a[i][j];
+        }
+    }
+    while (q--)
+    {
+        int x1, y1, x2, y2;
+        scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
+        printf("%d\n", s[x2][y2] - s[x2][y1 - 1] - s[x1 - 1][y2] + s[x1 - 1][y1 - 1]);
+    }
+
+}
