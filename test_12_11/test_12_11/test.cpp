0cd84d7e6e308d0b59e1cb37e9f2c62a0bca9069
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-12-11 15:13:44 +0800

    2048. 下一个更大的数值平衡数

---
+++ test_12_11/test_12_11/test.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+class Solution {
+public:
+    bool isBalance(int x)
+    {
+        vector<int> count(10);
+        while (x > 0)
+        {
+            count[x % 10]++;
+            x = x / 10;
+        }
+        for (int d = 0; d < 10; d++)
+        {
+            if (count[d] > 0 && count[d] != d)
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+    int nextBeautifulNumber(int n) {
+        for (int i = n + 1; i <= 1224444; i++)
+        {
+            if (isBalance(i))
+            {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+};
