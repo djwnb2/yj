156007fd0a64e032dce346a70c7a78ab1a801130
dai-jiawei-is-a-handsome-force <1375083509@qq.com>
2023-12-21 15:27:39 +0800

    2828. 判别首字母缩略词

---
+++ test_12_21/test_12_21/test.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        if (s.size() != words.size())
+        {
+            return false;
+        }
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (words[i][0] != s[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
